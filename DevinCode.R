41789553d3b8eda9a447e60c0a850b4386d5814f
Devin <luckyfrost1@gmail.com>
2024-02-23 21:13:13 -0800

    Update DevinCode.R

--- DevinCode.R
+++ DevinCode.R
@@ -7,9 +7,21 @@ library(readr) # for reading files
 YelpData <- read_csv("Yelp Academic Dataset - Select CA Cities & Businesses.csv")
 View(Yelp_Academic_Dataset_Select_CA_Cities_Businesses)
 
-LatLong <- YelpData |> select(Name,Latitude,Longitude,Stars)
+LatLong <- YelpData |> select(Name, Latitude, Longitude, Stars, Categories) |> 
+  filter(Latitude < 35, Longitude < -119)
 
 
 # This code should work, converts to sf data and views
 sf_LatLong <- st_as_sf(LatLong, coords = c("Longitude", "Latitude"),  crs = 4326)
-mapview(sf_LatLong, map.types = "CartoDB.DarkMatter") 
+
+mapview(sf_LatLong, map.types = "CartoDB.DarkMatter", zcol="Stars") 
+
+##
+## New Section
+##  Working on correlation
+
+
+
+
+
+
